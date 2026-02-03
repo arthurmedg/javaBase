d7364769e0667fe1a688dc4e51acd068b48f14aa
medina <arthurmedg@gmail.com>
2026-02-03 09:51:59 -0300

    Add files via upload

---
+++ aula02/OperacoesAritmeticas.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
+
+    System.out.println(2 * 6 / 3);
+    System.out.println(3 + 2 * 4);
+    System.out.println((3 + 2) * 4);
+    System.out.println(60 / (3 + 2) * 4);
+    System.out.println(60 / ((3 + 2) * 4));
+}
