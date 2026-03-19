d3bafb0880a2581f130f03629fd016e3513a9074
medina <arthurmedg@gmail.com>
2026-03-18 21:47:07 -0300

    Delete BatalhaPersonagens directory

--- BatalhaPersonagens/src/Guerreiro.java
+++
@@ -1,8 +0,0 @@
-public class Guerreiro extends Personagem{
-    public Guerreiro(String nome, int dano, int ataque, int defesa) {
-        super(nome, dano, ataque, defesa);
-    }
-
-    public Guerreiro() {
-    }
-}
--- BatalhaPersonagens/src/Main.java
+++
@@ -1,8 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  Mago mago = new Mago("teste", 10, 10, 10);
-
-  System.out.println(mago.mostrarStatus());
-
-}
