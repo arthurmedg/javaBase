40eb74c440b1444729846d8224650cb1d9da0afb
medina <arthurmedg@gmail.com>
2026-03-19 10:59:15 -0300

    Delete BatalhaPersonagens directory

--- BatalhaPersonagens/src/Arqueiro.java
+++
@@ -1,8 +0,0 @@
-public class Arqueiro extends Personagem {
-    public Arqueiro(String nome, int vida, int ataque, int defesa) {
-        super(nome, vida, ataque, defesa);
-    }
-
-    public Arqueiro() {
-    }
-}
