2f54f2c34112cbcdd66fe4deca9828231137bae2
medina <arthurmedg@gmail.com>
2026-04-07 10:15:13 -0300

    Delete AtvdSistemaPagamentosDigitais/SistemadePagamentosDigitais directory

--- AtvdSistemaPagamentosDigitais/SistemadePagamentosDigitais/src/BoletoBancario.java
+++
@@ -1,9 +0,0 @@
-package AtvdSistemaPagamentosDigitais.SistemadePagamentosDigitais.src;
-
-import java.time.LocalDateTime;
-
-public class BoletoBancario extends Pagamento {
-    public BoletoBancario(double valor, String status, LocalDateTime dataHoraPagamento) {
-        super(valor, status, dataHoraPagamento);
-    }
-}
--- AtvdSistemaPagamentosDigitais/SistemadePagamentosDigitais/src/CartaoCredito.java
+++
@@ -1,9 +0,0 @@
-package AtvdSistemaPagamentosDigitais.SistemadePagamentosDigitais.src;
-
-import java.time.LocalDateTime;
-
-public class CartaoCredito extends Pagamento {
-    public CartaoCredito(double valor, String status, LocalDateTime dataHoraPagamento) {
-        super(valor, status, dataHoraPagamento);
-    }
-}
